568b2848fcb39f498a1b97443e3bcfb6e13c9dc4
Kristiyan Tsvetanov <kristiyan.tsvetanov@paysafe.com>
2026-05-22 12:15:47 +0300

    Add missing REST models

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,3 +1,8 @@
 export type { MemberBase } from "./member_base";
 export type { MemberResponse } from "./member_response";
-export type { ErrorResponse } from "./error_response";
+export type { StatusResponse as ErrorResponse } from "./status_response";
+export type { BookBase } from "./book_base";
+export type { BookResponse } from "./book_response";
+export type { BookIssueBase } from "./book_issue_base";
+export type { BookIssueContent } from "./book_issue_content";
+export type { BookIssueResponse } from "./book_issue_response";
