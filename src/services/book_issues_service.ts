55499d5d7a9f2ffba884758b227b3195e99b0013
Hristo Velev <hristo.velev@paysafe.com>
2026-05-22 14:35:32 +0300

    added validation to book issues service

--- src/services/book_issues_service.ts
+++ src/services/book_issues_service.ts
@@ -8,8 +8,18 @@ import {
     deleteBookIssueFromDB
 } from "../db/db";
 
+import { 
+    validate_memberid, 
+    validate_bookid, 
+    validate_member_issue_count,
+    validate_bookissued 
+} from "../validations/validations";
+
 function createBookIssue(book_issue: BookIssueBase): BookIssue {
-    
+    validate_memberid(book_issue.memberId);
+    validate_bookid(book_issue.bookId);
+    validate_member_issue_count(book_issue.memberId);
+    validate_bookissued(book_issue.bookId);
     return createBookIssueInDB(book_issue);
 }
 
@@ -22,6 +32,7 @@ function getBookIssueByMemberId(memberid: number): BookIssue[] {
 }
 
 function deleteBookIssue(id: number): void {
+    validate_bookissued(id);
     deleteBookIssueFromDB(id);
 }
 
