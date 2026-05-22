b148fe5459bce471cddd37dfd4439a4b784c6ec5
Hristo Velev <hristo.velev@paysafe.com>
2026-05-22 15:20:40 +0300

    fixed imports and adds and .use

--- src/rest/app.ts
+++ src/rest/app.ts
@@ -1,10 +1,14 @@
 import { Elysia } from "elysia"
 import { memberApi } from "./member_api"
+import { bookApi } from "./book_api"
+import { bookIssuesApi } from "./book_issues_api"
 
 let port: number = 3000
 
 const app = new Elysia()
     .use(memberApi)
+    .use(bookApi)
+    .use(bookIssuesApi)
     .listen(port)
 
 console.log(`Server is running on ${app.server?.hostname}:${app.server?.port}`)
