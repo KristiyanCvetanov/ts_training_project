e50e645e087b5a0f7e5f705822987ee4e1b5d71d
Hristo Velev <hristo.velev@paysafe.com>
2026-05-22 11:41:40 +0300

    table foregin keys, functions added

--- src/db/db.ts
+++ src/db/db.ts
@@ -11,8 +11,12 @@ const db = new Database('library.db');
 
 function table_create():void{
     db.query('CREATE TABLE IF NOT EXISTS books (bookid INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, subject TEXT, author TEXT, language TEXT)').run();
-    db.query('CREATE TABLE IF NOT EXISTS book_issue (issueid INTEGER PRIMARY KEY AUTOINCREMENT, bookid INTEGER, memberid INTEGER, issue_date TEXT, return_date TEXT)').run();
+    db.query('CREATE TABLE IF NOT EXISTS book_issue (issueid INTEGER PRIMARY KEY AUTOINCREMENT, bookid INTEGER foreign key references books(bookid), memberid INTEGER foreign key references members(memberid), issue_date TEXT, return_date TEXT)').run();
     db.query('CREATE TABLE IF NOT EXISTS members (memberid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, phone TEXT, address TEXT)').run();
 }
 
 table_create();
+
+
+
+export {db, table_create};
