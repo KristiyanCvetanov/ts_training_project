48e4f65c91e4224989df08e8029d6b3dd52d7694
Hristo Velev <hristo.velev@paysafe.com>
2026-05-22 12:46:32 +0300

    fixed missing export

--- src/validations/validations.ts
+++ src/validations/validations.ts
@@ -119,4 +119,4 @@ function bookissue_validate_member_issue_count(memberid: number): void {
     }
 }
 
-export { validation_member, validation_book, validation_book_issue, bookissue_validate_memberid, bookissue_validate_member_issues, bookissue_validate_bookid };
+export { validation_member, validation_book, validation_book_issue, bookissue_validate_memberid, bookissue_validate_member_issues, bookissue_validate_bookid, bookissue_validate_member_issue_count };
